5b8758a0a5ec72b1d9f16676ec7da6084304a90c
elburg <berge61319@loswego.k12.or.us>
2021-03-18 22:02:56 +0000

    better build system. 3De still does nothing.

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-javac -classpath .:/run_dir/junit-4.12.jar:target/dependency/* -d main.java
+cd ~/3De
+echo "removing prebuilt classes"
+rm main.class
+echo "building 3De"
+javac main.java
+echo "starting 3De"
+java main
--- main.java
+++ main.java
@@ -1,8 +1,8 @@
 /**
  * this is the main controller for 3De
  */
-class HelloWorldApp {
+class main {
     public static void main(String[] args) {
-        System.out.println("3De"); // Display the string.
+        System.out.println("yay it werks! :3"); // Display the string.
     }
 }
