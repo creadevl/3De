085838110088d44fa03fd265804b5b3a0982e6fb
elburg the polygonned potato <berge61319@loswego.k12.or.us>
2021-03-18 21:50:04 +0000

    new stuff

---
+++ main.java
@@ -0,0 +1,8 @@
+/**
+ * this is the main controller for 3De
+ */
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("3De"); // Display the string.
+    }
+}
