4c11ca3094073a663a9005330cc80b46e0bf5e61
elburg <berge61319@loswego.k12.or.us>
2021-03-19 03:28:02 +0000

    v0.0.0 new startup string about function does not work

--- main.java
+++ main.java
@@ -2,10 +2,20 @@
  * this is the main controller for 3De
  */
 class main {
-    public static void main(String[] args) {
-        System.out.println("yay it werks! :3"); // Display the string.
-    }
-    public static void about() {
-      System.out.println("insert ~/3De/text/about.text here.");
-    }
+  public static void main(String[] args) {
+      System.out.println("3De -- Powered by Java.\nVersion 0.0.0\nCreated by Creative Development [ https://github.com/creadevl ]\n"); // Display the string.
+      try {
+        if (args[0] == "about"){
+          System.out.println("insert ~/3De/text/about.text here.");
+        }
+      } catch (java.lang.ArrayIndexOutOfBoundsException e){
+        System.err.println("[MAIN:Error]:No commands detected. Exiting...");
+        System.exit(0);
+      } finally {
+        if (args[0] == "about"){
+          System.out.println("insert ~/3De/text/about.text here.");
+        }
+      }
+      
+  }
 }
