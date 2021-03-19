384475e2f8499aa1607c9a5be16a3ab18bedb4b6
elburg the polygonned potato <berge61319@loswego.k12.or.us>
2021-03-19 00:52:37 +0000

    tried to make an about function in the main controller

--- main.java
+++ main.java
@@ -5,4 +5,7 @@ class main {
     public static void main(String[] args) {
         System.out.println("yay it werks! :3"); // Display the string.
     }
+    public static void about() {
+      System.out.println("insert ~/3De/text/about.text here.");
+    }
 }
