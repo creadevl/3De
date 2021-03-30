862c48033b2b31844e857a2e708b883c15e565fb
elburg the polygonned potato <berge61319@loswego.k12.or.us>
2021-03-30 01:36:56 +0000

    new concept art

--- out/production/3De/README.md
+++
@@ -1,2 +0,0 @@
-# 3De (3D engene)
-A 3D Game Engine made in Java... For Java.
--- out/production/3De/build.sh
+++
@@ -1,7 +0,0 @@
-cd ~/3De
-echo "removing prebuilt classes"
-rm main.class
-echo "building 3De"
-javac main.java
-echo "starting 3De"
-java main
