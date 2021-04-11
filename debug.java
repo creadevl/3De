79cfac45016a69a543f64e6c02900e726a459a32
polygonnedpotato <cuteiethecorgi@gmail.com>
2021-04-11 15:10:59 -0700

    new debug module moved build scripts

--- debug.java
+++ debug.java
@@ -1,3 +1,15 @@
 public class debug {
-    
+    public static void FatalEscape(String SourceModule, String Cause, String DumpSeed){
+        switch(DumpSeed) {
+            case "0a":
+                // code block
+                break;
+            case "0b":
+                // code block
+                break;
+            default:
+                // code block
+        }
+
+    }
 }
