6e6ec8eb048d52356331164037e6d3e6eb84deba
polygonnedpotato <cuteiethecorgi@gmail.com>
2021-04-12 09:19:19 -0700

    working on config and debug modules

--- config.java
+++ config.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 public class config {
-    
+   public boolean does3DeFolderExist(){
+        
+   }  
 }
--- debug.java
+++ debug.java
@@ -1,5 +1,13 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileDescriptor;
+import java.time.*;
+import java.util.logging.*;
 public class debug {
     public static void FatalEscape(String SourceModule, String Cause, String DumpSeed){
+        int lastDumpId = new File("~/.3De/Data/Dumpfiles/").listFiles().length;
+        
         switch(DumpSeed) {
             case "0a":
                 // code block
@@ -9,6 +17,7 @@ public class debug {
                 break;
             default:
                 // code block
+            System.exit(0);
         }
 
     }
